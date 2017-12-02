4cd2e6688fbaea6240f86a08046a6eea63b3981e
Anton <it2015maistrenko@gmail.com>
2017-12-02 23:21:02 +0200

    Fix url endpoint for PATCH profile.

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ from accounts import views
 urlpatterns = [
     url(r'^signup', RegisterView.as_view(), name='account_signup'),
     url(r'^login', LoginView.as_view(), name='account_login'),
-    url(r'^profile/(?P<uuid>[-:\w]+)/$', views.UserProfileView.as_view(), name='user_profile'),
+    url(r'^profile/(?P<uuid>[-:\w]+)$', views.UserProfileView.as_view(), name='user_profile'),
     url(r'^account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(),
         name='account_confirm_email'),
 ]
