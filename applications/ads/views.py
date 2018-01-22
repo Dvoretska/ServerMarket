a17b1fe6a7f52bb55389c4378c9c71535d5b9436
Anton <it2015maistrenko@gmail.com>
2018-01-22 23:54:19 +0200

    Change response for categories

--- applications/ads/views.py
+++ applications/ads/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
 
 from applications.ads.models import Category
 from applications.ads.serializers import CategorySerializer
@@ -8,3 +9,7 @@ class CategoriesView(ListAPIView):
 
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        qs = self.filter_queryset(self.get_queryset())
+        return Response([category[0] for category in qs.values_list('name')])
