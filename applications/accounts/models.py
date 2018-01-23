23215d1e6319062a8b008cefca2f97df2d5b0478
Anton <it2015maistrenko@gmail.com>
2018-01-23 21:45:33 +0200

    Default values for city and country.

--- applications/accounts/models.py
+++ applications/accounts/models.py
@@ -6,5 +6,5 @@ from django.contrib.auth.models import AbstractUser
 
 class UserProfile(AbstractUser):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    country = models.CharField(max_length=255, null=True, blank=True)
-    city = models.CharField(max_length=255, null=True, blank=True)
+    country = models.CharField(max_length=255, default='', blank=True)
+    city = models.CharField(max_length=255, default='', blank=True)
