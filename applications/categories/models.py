507c40690d0c2a35b44a75783d7f91f7716df8d1
Anton <it2015maistrenko@gmail.com>
2018-04-01 17:39:30 +0300

    Add slug to categories.

--- applications/categories/models.py
+++ applications/categories/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from model_utils.models import TimeStampedModel
+from autoslug import AutoSlugField
 
 
 class Category(models.Model):
 
     name = models.CharField(_('Category'), max_length=255)
+    slug = AutoSlugField(null=True, default=None, unique=True, populate_from='name')
 
     class Meta:
         verbose_name_plural = _('Categories')
--- applications/categories/views.py
+++ applications/categories/views.py
@@ -1,6 +1,5 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 
 from .models import Category
 from .serializers import CategorySerializer
@@ -11,7 +10,3 @@ class CategoriesListView(ListAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
     permission_classes = (AllowAny,)
-
-    def get(self, request, *args, **kwargs):
-        qs = self.filter_queryset(self.get_queryset())
-        return Response([category[0] for category in qs.values_list('name')])
