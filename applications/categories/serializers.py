d316649fc65bf53e14e960ac08b2ff92a5a80f33
Anton <it2015maistrenko@gmail.com>
2018-04-02 23:14:00 +0300

    Option for filters.

--- applications/categories/serializers.py
+++ applications/categories/serializers.py
@@ -5,6 +5,13 @@ from .models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
 
+    count = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
-        fields = ('name', 'slug')
+        fields = ('name', 'slug', 'count')
+
+    @classmethod
+    def get_count(cls, obj):
+        return obj.categories.count()
+
