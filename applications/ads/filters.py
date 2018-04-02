ee3502c333c72a63b8170a61dfcdb00603ab4a7e
Anton <it2015maistrenko@gmail.com>
2018-04-02 20:26:04 +0300

    Add ListFilter for multiple category.

--- applications/ads/filters.py
+++ applications/ads/filters.py
@@ -2,10 +2,18 @@ from django_filters import rest_framework as filters
 
 from applications.ads.models import Ad
 
+from django_filters import Filter
+from django_filters.fields import Lookup
+
+
+class ListFilter(Filter):
+    def filter(self, qs, value):
+        return super().filter(qs, Lookup(value.split(','), 'in'))
+
 
 class AdFilter(filters.FilterSet):
 
-    category = filters.CharFilter(field_name='category__slug')
+    category = ListFilter(field_name='category__slug')
 
     class Meta:
         model = Ad
