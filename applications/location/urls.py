ac05ffae246b2e7043ffbc3231273819a75da697
Anton <it2015maistrenko@gmail.com>
2019-01-31 22:57:48 +0200

    Add cities search

--- applications/location/urls.py
+++ applications/location/urls.py
@@ -4,5 +4,6 @@ from applications.location.views import CountryView, CitiesView
 
 urlpatterns = [
     url(r'^countries/', CountryView.as_view(), name='countries'),
-    url(r'^(?P<code>[-:\w]+)/cities/$', CitiesView.as_view(), name='cities'),
+    url(r'^cities/(?P<search>[-:\w]+)/', CitiesView.as_view(), name='cities'),
+    # url(r'^(?P<code>[-:\w]+)/cities/$', CitiesView.as_view(), name='cities'),
 ]
--- applications/location/views.py
+++ applications/location/views.py
@@ -15,6 +15,28 @@ class CountryView(APIView):
         return Response({country.name: country.alpha_2.lower() for country in pycountry.countries})
 
 
+# class CitiesView(APIView):
+#
+#     CACHE_PREFIX = 'country_'
+#
+#     authentication_classes = ()
+#     permission_classes = ()
+#
+#     def get(self, request, code):
+#         cities = cache.get('{}{}'.format(self.CACHE_PREFIX, code.lower()))
+#         if not cities:
+#             cities = self._get_cities_by_code(code)
+#             if cities:
+#                 cache.set('{}{}'.format(self.CACHE_PREFIX, code.lower()), cities)
+#         return Response(cities)
+#
+#     @classmethod
+#     def _get_cities_by_code(cls, code):
+#         with open('applications/location/cities.json') as data_file:
+#             data = json.load(data_file)
+#         return [city['name'] for city in data if city["country"] == code.upper()]
+
+
 class CitiesView(APIView):
 
     CACHE_PREFIX = 'country_'
@@ -22,16 +44,16 @@ class CitiesView(APIView):
     authentication_classes = ()
     permission_classes = ()
 
-    def get(self, request, code):
-        cities = cache.get('{}{}'.format(self.CACHE_PREFIX, code.lower()))
-        if not cities:
-            cities = self._get_cities_by_code(code)
-            if cities:
-                cache.set('{}{}'.format(self.CACHE_PREFIX, code.lower()), cities)
-        return Response(cities)
+    def get(self, request, search):
+        return Response(self._get_cities_by_code(search.lower()))
 
-    @classmethod
-    def _get_cities_by_code(cls, code):
-        with open('applications/location/cities.json') as data_file:
+    @staticmethod
+    def _get_cities_by_code(search):
+        result = []
+        with open('applications/location/cities_ua.json') as data_file:
             data = json.load(data_file)
-        return [city['name'] for city in data if city["country"] == code.upper()]
+        for state in data['areas']:
+            for city in state['areas']:
+                if city['name'].lower().startswith(search):
+                    result.append(f'{city["name"]}, {state["name"]}')
+        return result
